63a1184f389dee8fea262876611097293c39d4e9
Eric Sorenson <eric.sorenson@dubharmonic.com>
2013-04-24 08:32:16 -0500

    wip

--- jquery-flipbook.js
+++ jquery-flipbook.js
@@ -1,11 +1,9 @@
 ;(function($, window, document, undefined) {
 
-  $.fn.flipbook = function() {
+  $.fn.flipbook2 = function() {
     var flipbook = this;
 
-    var height = flipbook.data('height') || '300';
-    var width = flipbook.data('width') || '200';
-    // var pageCount = flipbook.data('pages') || '2';
+    var pagesPer = flipbook.data('pages') || '2';
 
     var pages;
     var oddPages;
@@ -15,7 +13,7 @@
     var pageCount;
 
     var init = function() {
-      if(flipbook.find('.page').length % 2 == 1) {
+      if(flipbook.find('.page').length % 2 === 1) {
         flipbook.append('<div class="page"></div>');
       }
 
@@ -24,21 +22,24 @@
       evenPages = flipbook.find('.page:nth-child(even)');
       pageCount = pages.length;
 
-      flipbook.css('width', parseInt(width, 10)*2);
-      pages.css('height', height);
-      pages.css('width', width);
+      var width = flipbook.css('width');
+      pages.css('width', parseInt(width,10)/2);
 
       pages.each(function(index, page) {
+        $(page).css('z-index', pageCount - index)
         if(index > 1) {
-          $(page).addClass('flipped');
-
+          if(index%2 === 1) {
+            $(page).addClass('flipped');
+          }
+          // $(page).addClass('flipped');
+          // $(page).addClass('invisible');
         }
       });
     };
 
     var flipForward = function() {
       if(pageCount > (currentPage + 2)) {
-        // $(pages[currentPage]).toggleClass('flipped');
+        $(pages[currentPage]).toggleClass('flipped');
         $(pages[currentPage+1]).addClass('flipped');
         currentPage += 2;
         $(pages[currentPage]).removeClass('flipped');
@@ -70,5 +71,37 @@
     $('.flipbook-first-page').click(goToFirstPage);
 
   };
+
+  //////////////////
+
+  $.fn.flipbook = function() {
+    var flipbook = this;
+    var pages = flipbook.find('.page');
+    var pageCount = pages.length;
+
+    var flipForward = function() {
+      console.log('forward');
+    };
+
+    var flipBackward = function() {
+      console.log('backward');
+    };
+
+    flipbook.addClass('flipbook-single');
+
+    pages.each(function(index, page) {
+      $(page).css('z-index', pageCount - index);
+    });
+
+    $(pages).click(function(e) {
+      console.log(e.offsetX);
+      if(e.offsetX > flipbook.width()/2) {
+        flipForward();
+      } else {
+        flipBackward();
+      }
+    });
+
+  }
 
 })(jQuery, window, document);
